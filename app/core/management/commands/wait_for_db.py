aac63c9920ff6b06cfa9bdc557f22ab794762ff4
Ben Carson <ben03carson@gmail.com>
2023-12-28 23:44:06 +0300

    update wait_for_db code

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -3,6 +3,7 @@ Django command to wait for the database to be available.
 """
 import time
 
+from django.db import connections
 from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.db.utils import OperationalError
@@ -16,11 +17,12 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         """Entry point for command"""
         self.stdout.write('waiting for database')
-        db_up = False
-        while db_up is False:
+        db_conn = None
+        while not db_conn:
             try:
-                self.check(databases=['default'])
-                db_up = True
+                # self.check(databases=['default'])
+                # db_up = True
+                db_conn = connections['default']
             except (Psycopg2OpError, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -10,11 +10,11 @@ from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
 
-@patch('core.management.commands.wait_for_db.Command.check')
+
 class CommandTests(SimpleTestCase):
     """Test commands here."""
 
-    def test_wait_for_db_ready(self, patched_check):
+    def test_wait_for_db_ready(self):
         """Test waiting for database if ready."""
         patched_check.return_value = True
 
